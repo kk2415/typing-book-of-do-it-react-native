5ea0d87c819d9d6460a060d47d51d9a57e6fdda0
Kyunghee Kim <kkh2415@naver.com>
2022-12-19 11:02:50 +0900

    주석 추가, 클린 코드

--- ch02/ch02_4/App.tsx
+++ ch02/ch02_4/App.tsx
@@ -5,10 +5,14 @@ import {
   StatusBar,
   StyleSheet,
   Text,
+  TextInput,
   useColorScheme,
   View,
   Button,
   Alert,
+  Touchable,
+  TouchableOpacity,
+  TouchableHighlight,
 } from 'react-native';
 
 import {
@@ -19,11 +23,28 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
+const onPress = () => Alert.alert('home pressed', 'message')
+
 const App = () => {
   return (
     <SafeAreaView>
       <Button title="home" color="blue" onPress={() => console.log('home pressed.')}></Button>
       <Button title="Alert" color="blue" onPress={() => Alert.alert('home pressed.', 'message')}></Button>
+      <TouchableOpacity onPress={onPress}>
+        <Text>TouchableOpacity</Text>
+      </TouchableOpacity>
+      <TouchableHighlight onPress={onPress}>
+        <Text>TouchableHighlight</Text>
+      </TouchableHighlight>
+      <Text onPress={onPress}>Press Me</Text>
+
+      <TextInput placeholder="텍스트를 입력해주세요" 
+                  onChangeText={(text: string) => console.log(text)}
+                  onFocus={() => console.log('onFocus')}
+                  onBlur={() => console.log('onBlur')}
+                  onEndEditing={() => console.log('onEndEditing')}
+                  >
+      </TextInput>
     </SafeAreaView>
   );
 };
