8110befd9988da1985746a3b32c0c6ec99e7b835
Kyunghee Kim <kkh2415@naver.com>
2022-12-19 11:03:34 +0900

    챕터 3-2 추가

--- ch03/ch03_2/App.tsx
+++ ch03/ch03_2/App.tsx
@@ -1,24 +1,14 @@
-import React, {type PropsWithChildren} from 'react';
+import React from 'react';
 import {
   SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
   View,
   Platform,
-  Dimensions
+  Dimensions,
 } from 'react-native';
 
-import {
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-let {width, height} = Dimensions.get('window')
+let {width, height} = Dimensions.get('window');
 
 const App = () => {
   return (
@@ -26,18 +16,29 @@ const App = () => {
       <Text style={[styles.text]}>os : {Platform.OS}</Text>
       <Text style={[styles.text]}>width : {width}</Text>
       <Text style={[styles.text]}>height : {height}</Text>
-      <View style={[styles.box, {borderRadius: 10}]}></View>
-      <View style={[styles.box, styles.border]}></View>
-      <View style={[styles.box, styles.border, {borderRadius: 10}]}></View>
+      <View style={[styles.box, {borderRadius: 10}]} />
+      <View style={[styles.box, styles.border]} />
+      <View style={[styles.box, styles.border, {borderRadius: 10}]} />
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  safeAreaView: {backgroundColor: 'blue', height: '50%', flex: 1, margin: '10%'},
+  safeAreaView: {
+    backgroundColor: 'blue',
+    height: '50%',
+    flex: 1,
+    margin: '10%',
+  },
   text: {fontSize: 20, marginBottom: 10, marginLeft: 50, marginTop: 20},
-  box: {width: '70%', height: 100, backgroundColor: 'white', marginBottom: 10, marginLeft: Platform.select({ios: 20, android: 0})},
-  border: {borderWidth: 10, borderColor: 'yellow'}
+  box: {
+    width: '70%',
+    height: 100,
+    backgroundColor: 'white',
+    marginBottom: 10,
+    marginLeft: Platform.select({ios: 20, android: 0}), //운영체제 별로 다른 값 적용
+  },
+  border: {borderWidth: 10, borderColor: 'yellow'},
 });
 
 export default App;
